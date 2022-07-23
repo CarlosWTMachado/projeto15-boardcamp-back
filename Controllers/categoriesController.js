f6ae1b55cd4df3d69af76ed8b75d24c844de4bab
CarlosWTMachado <carloswtmachado@gmail.com>
2022-07-23 09:58:54 -0300

    feature categories

--- Controllers/categoriesController.js
+++ Controllers/categoriesController.js
@@ -1,8 +1,10 @@
 import db from '../dbStrategy/db.js';
+//import { stripHtml } from "string-strip-html";
 
 export async function GetCategories(_, res) {
 	try {
-		const {rows: categories} = await db.query('SELECT * FROM categories');
+		const query = "SELECT * FROM categories;";
+		const {rows: categories} = await db.query(query);
 		res.send(categories);
 	} catch (error) {
 		return res.status(500).send(error);
@@ -12,7 +14,8 @@ export async function GetCategories(_, res) {
 export async function PostCategories(req, res) {
 	const {name} = req.body;
 	try {
-		await db.query("INSERT INTO categories (name) VALUES ($1);", [name]);
+		const query = "INSERT INTO categories (name) VALUES ($1);";
+		await db.query(query, [name]);
 		res.sendStatus(201);
 	} catch (error) {
 		return res.status(500).send("error");
--- Middlewares/categoriesMiddleware.js
+++ Middlewares/categoriesMiddleware.js
@@ -1,18 +1,16 @@
-import joi from 'joi';
+import categorySchema from '../Schemas/schemas.js';
 import db from '../dbStrategy/db.js';
 
 export function ValidaPostCategories(req, res, next) {
-	const schema = joi.object({
-		name: joi.string().required()
-	});
-	const validation = schema.validate({name: req.body.name});
+	const validation = categorySchema.validate({name: req.body.name});
 	if(validation.error) return res.status(400).send(validation.error.details);
 	next();
 }
 
 export async function VerificaPostCategories(req, res, next) {
 	try {
-		const {rows: category} = await db.query("SELECT * FROM categories WHERE name = $1;", [req.body.name]);
+		const query = "SELECT * FROM categories WHERE name = $1;";
+		const {rows: category} = await db.query(query, [req.body.name]);
 		if(category.length > 0) return res.sendStatus(409);
 		next();
 	} catch (error) {
